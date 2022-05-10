cb46d4ee78d81fcc8d40eb697fe9384373fb2c64
Anita Caron <anitac@ebi.ac.uk>
2022-05-10 18:09:19 +0100

    load datasource, terms and mappings to neo4j

--- oxo-loader/OxoNeo4jLoader.py
+++ oxo-loader/OxoNeo4jLoader.py
@@ -19,7 +19,7 @@ class Neo4jOxOLoader:
         # else:
         #     config = ConfigParser()
         #     config.read(sys.argv[1])
-        print("changed 2")
+
         parser = OptionParser()
         parser.add_option("-d","--datasources",  help="load the datasource file")
         parser.add_option("-t","--terms", help="load the term file")
@@ -27,7 +27,7 @@ class Neo4jOxOLoader:
         parser.add_option("-W","--wipe", action="store_true", dest="wipe", help="wipe the neo4j database")
         parser.add_option("-c","--config", help="config file", default="config.ini")
 
-        (options, args) = parser.parse_args()
+        options, _ = parser.parse_args()
 
         config = ConfigParser()
         config.read(options.config)
@@ -37,14 +37,14 @@ class Neo4jOxOLoader:
         except:
             print("No config found, please supply a config.ini using -c")
             exit (1)
-        neoUser = config.get("Basics", "neoUser")
-        neoPass = config.get("Basics", "neoPass")
+        neo_user = config.get("Basics", "neoUser")
+        neo_pass = config.get("Basics", "neoPass")
 
-        driver = GraphDatabase.driver(uri, auth=basic_auth(neoUser, neoPass), encrypted=False)
+        driver = GraphDatabase.driver(uri, auth=basic_auth(neo_user, neo_pass), encrypted=False)
         self.session = driver.session()
 
-        # self.session.run("CREATE CONSTRAINT ON (i:Term) ASSERT i.curie IS UNIQUE")
-        # self.session.run("CREATE CONSTRAINT ON (i:Datasource) ASSERT i.prefix IS UNIQUE")
+        self.session.run("CREATE CONSTRAINT IF NOT EXISTS ON (i:Term) ASSERT i.curie IS UNIQUE")
+        self.session.run("CREATE CONSTRAINT IF NOT EXISTS ON (i:Datasource) ASSERT i.prefix IS UNIQUE")
 
         if options.wipe:
             while self.deleteMappings() > 0:
@@ -64,11 +64,11 @@ class Neo4jOxOLoader:
             print("Datasources deleted!")
 
         if options.datasources:
-            self.loadDatasources(options.datasources)
+            self.load_datasources(options.datasources)
         if options.terms:
-            self.loadTerms(options.terms)
+            self.load_terms(options.terms)
         if options.mappings:
-            self.loadMappings(options.mappings)
+            self.load_mappings(options.mappings)
 
 
     def deleteMappings(self):
@@ -91,40 +91,63 @@ class Neo4jOxOLoader:
         for record in result:
             return record["count"]
 
-    def loadTerms(self, terms):
+    def load_terms(self, terms):
         print("Loading terms from "+terms+"...")
 
-        loadTermsCypher = "USING PERIODIC COMMIT 10000 LOAD CSV WITH HEADERS FROM 'file:///"+terms+"""' AS line
-                        MATCH (d:Datasource {prefix : line.prefix})
-                        WITH d, line
-                        MERGE (t:Term { curie: line.curie})
-                        SET t.id = line.identifier, t.label = line.label, t.uri = line.uri
-                        with t,d
-                        CREATE (t)-[:HAS_SOURCE]->(d)"""
-        result = self.session.run(loadTermsCypher)
-        print(result.summary())
+        load_subject_terms_cypher = """
+            USING PERIODIC COMMIT 10000 LOAD CSV WITH HEADERS FROM 'file:///"""+terms+"""' AS row
+            FIELDTERMINATOR '\t'
+            WITH row
+            MERGE (t1:Term { curie: row.subject_id})
+            SET t1.label = CASE trim(row.subject_label) WHEN "" THEN null ELSE row.subject_label END
+            SET t1.category = CASE trim(row.subject_category) WHEN "" THEN null ELSE row.subject_category END
+            WITH row
+            MERGE (t2:Term { curie: row.object_id})
+            SET t2.label = CASE trim(row.object_label) WHEN "" THEN null ELSE row.object_label END
+            SET t2.category = CASE trim(row.object_category) WHEN "" THEN null ELSE row.object_category END
+        """
 
-    def loadMappings(self, mappings):
+        load_terms_souce_cypher = """
+          USING PERIODIC COMMIT 10000 LOAD CSV WITH HEADERS FROM 'file:///"""+terms+"""' AS row
+          FIELDTERMINATOR '\t'
+          MATCH (t1:Term { curie: row.subject_id}), (t2:Term { curie: row.object_id})
+          MATCH (d1:Datasource { prefix: row.subject_source}), (d2:Datasource { prefix: row.object_source})
+          MERGE (t1)-[:HAS_SOURCE]->(d1)
+          MERGE (t2)-[:HAS_SOURCE]->(d2)
+        """
+
+        result = self.session.run(load_subject_terms_cypher)
+        print(result.consume())
+        result = self.session.run(load_terms_souce_cypher)
+        print(result.consume())        
+
+    def load_mappings(self, mappings):
         print("Loading mappings from "+mappings+"...")
-        loadMappingsCypher = "USING PERIODIC COMMIT 10000 LOAD CSV WITH HEADERS FROM 'file:///"+mappings+"""' AS line
-                        MATCH (f:Term { curie: line.fromCurie}),(t:Term { curie: line.toCurie})
-                        WITH f,t,line
-                        CREATE (f)-[m:MAPPING { sourcePrefix: line.datasourcePrefix, datasource: line.datasource, sourceType: line.sourceType, scope: line.scope, date: line.date}]->(t)"""
+        load_mappings_cypher = """
+            USING PERIODIC COMMIT 10000 LOAD CSV WITH HEADERS FROM 'file:///"""+mappings+"""' AS row
+            FIELDTERMINATOR '\t'
+            WITH row
+            MATCH (subject:Term { curie : row.subject_id}), (object:Term { curie: row.object_id})
+            MERGE (subject)-[m:MAPPING { predicate: row.predicate_id, matchType: row.match_type, mappingTool: row.mapping_tool, confidence: row.confidence, matchCategory: row.match_category}]->(object)
+            SET m.matchString = CASE trim(row.match_string) WHEN "" THEN null ELSE row.match_string END
+          """
 
-        result = self.session.run(loadMappingsCypher)
-        print(result.summary())
+        result = self.session.run(load_mappings_cypher)
+        print(result.consume())
 
-    def loadDatasources(self, datasources):
-        print("Loading datasrouces from " + datasources + " ...")
-        loadDatasourcesCypher = """
-            LOAD CSV WITH HEADERS FROM 'file:///"""+datasources+"""' AS line
-            WITH line
-            MERGE (d:Datasource {prefix : line.prefix})
-            WITH d, line
-            SET d.preferredPrefix = line.prefix, d.name = line.title, d.description = line.description, d.versionInfo = line.versionInfo, d.idorgNamespace = line.idorgNamespace, d.licence = line.licence, d.sourceType = line.sourceType, d.alternatePrefix = split(line.alternatePrefixes,",")
-            """
-        result = self.session.run(loadDatasourcesCypher)
-        print(result.summary())
+    def load_datasources(self, datasources):
+        print("Loading datasources from " + datasources + " ...")
+        load_datasources_cypher = """
+            LOAD CSV WITH HEADERS FROM 'file:///"""+datasources+"""' AS row
+            FIELDTERMINATOR '\t'
+            WITH row
+            MERGE (d1:Datasource {prefix : row.subject_source})
+            MERGE (d2:Datasource {prefix : row.object_source})
+        """
+        # WITH d, line
+        #SET d.preferredPrefix = line.prefix, d.name = line.title, d.description = line.description, d.versionInfo = line.versionInfo, d.idorgNamespace = line.idorgNamespace, d.licence = line.licence, d.sourceType = line.sourceType, d.alternatePrefix = split(line.alternatePrefixes,",")
+        self.session.run(load_datasources_cypher)
+      
 
 if __name__ == '__main__':
     Neo4jOxOLoader()
